88baafd92501fa7e1e07956f97a8ca6c754ff56a
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-05-19 12:34:37 -0700

    Cambios en la interfaz de stock. Mejora en informaciones. Cambios en el carrito de compra.

--- Frontend/src/app/views/stock/stockcards/stockcards.component.ts
+++ Frontend/src/app/views/stock/stockcards/stockcards.component.ts
@@ -1,11 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {Venta} from "../stock.component";
 import {CartService} from "../cartservice.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-stockcards',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './stockcards.component.html',
   styleUrls: ['./stockcards.component.css']
 })
@@ -13,9 +16,15 @@ export class StockcardsComponent {
   @Input() venta!: Venta;
 
   constructor(private cartService: CartService) { }
+  added: boolean = false;
 
   addToCart(venta: Venta) {
     this.cartService.addToCart(venta);
     console.log(venta.titulo + ' added to cart')
+  }
+
+  setadded(venta: Venta){
+    this.addToCart(venta)
+    this.added = !this.added;
   }
 }
