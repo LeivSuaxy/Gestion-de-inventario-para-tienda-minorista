cc7b90721e3a12fcf0d232bc23484cba5786706e
AGR55 <gonzalezreyesadriano@gmail.com>
2024-05-25 03:58:12 +0200

    prueba

--- Backend/Backend/api/models.py
+++ Backend/Backend/api/models.py
@@ -7,7 +7,7 @@ from django.db import models
 # Create your models here.
 class StockElement(models.Model):
     id = models.IntegerField(primary_key=True)
-    image = models.ImageField(upload_to='stock', null=True, blank=True)
+    image = models.ImageField(upload_to='stock')
     name = models.CharField(max_length=100)
     price = models.FloatField()
     description = models.TextField()
