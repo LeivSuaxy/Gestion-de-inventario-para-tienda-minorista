0ccba04dfeb11e26f65cb4b46fb7e452e6a30590
Javier Leiva Suárez <127800936+LeivSuaxy@users.noreply.github.com>
2024-05-26 06:33:56 -0700

    Delete Backend/Backend directory
    Delete Backend Directory

--- Backend/Backend/api/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import StockElement
-
-
-class StockElementSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StockElement
-        fields = ['image', 'name', 'price', 'description', 'stock']
--- Backend/Backend/api/views.py
+++
@@ -1,9 +0,0 @@
-from .models import StockElement
-from .serializer import StockElementSerializer
-from rest_framework import viewsets
-
-
-# Create your views here.
-class StockElementViewSet(viewsets.ModelViewSet):
-    queryset = StockElement.objects.all()
-    serializer_class = StockElementSerializer
