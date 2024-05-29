74cbc5b5c01f6be2b341bce53b0371996b4b7ec3
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-05-29 06:33:32 -0700

    Revert "Change gitignore"
    This reverts commit 14f75ae4

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '018900149913',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
