60b32a2ad78ecdc3a03426f6e4b9ee11d5af8ae4
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-06-04 20:06:10 -0700

    Servicio de API para registro y frontend de registro hecho

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'corsheaders',
-    "login.apps.LoginConfig"
+    "login",
 ]
 
 MIDDLEWARE = [
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SBD',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': '018900149913',
         'HOST': 'localhost',
         'PORT': '5432',
     }
