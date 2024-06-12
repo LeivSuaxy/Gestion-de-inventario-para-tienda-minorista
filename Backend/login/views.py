504e54b728180229fe88d1e87fc41f5abd6a3d1b
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-06-11 23:04:48 -0700

    Realizado primera consulta SQL manual para cumplimiento de la tarea asignada

--- Backend/login/views.py
+++ Backend/login/views.py
@@ -4,17 +4,49 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.views import Token
 from .serializer import UserSerializer
-from Backend.crudDB import CrudDB as db
+from Backend.crudDB import CrudDB
 
 
 # Create your views here.
 
 @api_view(['POST'])
 def register(request):
-    print(request.data)
-    dp = db()
-    dp.connect_test()
-    return Response({'Hello'}, status=200)
+    """
+    This view handles the registration of a new user
+
+    It expects a POST request with 'username' and 'password' in the request data.
+    If the username or password is missing, it returns a 400 error.
+    If the username already exists in the database it returns a 409 error.
+    If the registration is successful, it returns a 200 status.
+
+    :param request: The request object.
+    :return: A response object with the status of the operation.
+    """
+
+    # Create a new instance of the CrudDB class
+    db = CrudDB()
+
+    # Try to get the username and password from the request
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    # If the username or password is not in the request, return a 400 error
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'}, status.HTTP_400_BAD_REQUEST)
+
+    # Call the register_user method of the CrudDB instance with the username and password
+    responsedb = db.register_user(username=username, password=password)
+
+    # If the registration is successful, return a 200 status
+    if responsedb == 200:
+        return Response({'status': 'Success'}, status.HTTP_200_OK)
+    # If the user already exists, return a 409 status
+    elif responsedb == 409:
+        return Response({'status': 'This user already exists'}, status.HTTP_409_CONFLICT)
+    # If there is any other error, return a 400 error
+    else:
+        return Response({'status': 'Error'}, status.HTTP_400_BAD_REQUEST)
+
 
 
 @api_view(['POST'])
