2e88abe97fee8a8982c7894234f35c139817f0e6
AGR55 <gonzalezreyesadriano@gmail.com>
2024-06-21 08:27:15 +0200

    menu arreglado

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {ModalwithshopComponent} from "../views/stock/modalwithshop/modalwithshop.component";
 import {Router} from "@angular/router";
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -21,9 +22,22 @@ import {Router} from "@angular/router";
 export class NavbarComponent {
   constructor(public router: Router) { }
 
+  @ViewChild('menuButton', { static: true }) menuButton!: ElementRef;
+  @ViewChild('menu', { static: true }) menu!: ElementRef;
+  @ViewChild('navbar', { static: true }) navbar!: ElementRef;
   menuOpen = false;
 
   toggleMenu() {
-      this.menuOpen = !this.menuOpen;
+    this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+      const rect = this.navbar.nativeElement.getBoundingClientRect();
+      this.menu.nativeElement.style.top = `${rect.bottom}px`;
+      this.menu.nativeElement.style.left = `${rect.left}px`;
+      this.menu.nativeElement.style.maxHeight = '500px'; // Ajusta la maxHeight a un valor grande cuando el menú está abierto
+    } else {
+      this.menu.nativeElement.style.maxHeight = '0'; // Ajusta la maxHeight a '0' cuando el menú está cerrado
+    }
   }
 }
+
+
