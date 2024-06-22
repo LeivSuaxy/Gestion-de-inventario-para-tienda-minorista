0d3471c21e8543c4a8a97dfe26041c083a6ddddf
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-06-21 22:08:03 -0700

    Iniciando proceso de enviar correos al cliente.

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -148,3 +148,11 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'elitestock970430@gmail.com'
+EMAIL_HOST_PASSWORD = 'elitestockcorreo01890014'
