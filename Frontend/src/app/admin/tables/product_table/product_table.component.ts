328755742bfc40e92ac6b7b6c1eca3c7bd6d744c
AGR55 <gonzalezreyesadriano@gmail.com>
2024-06-22 20:00:28 +0200

    tabla empleados

--- Frontend/src/app/admin/tables/product_table/product_table.component.ts
+++ Frontend/src/app/admin/tables/product_table/product_table.component.ts
@@ -5,28 +5,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import { TablesComponent } from '../tables.component';
 import { StockComponent } from '../../../views/stock/stock.component';
 import { Venta} from "../../../views/stock/cartservice.service";
-import {CartService} from "../../../views/stock/cartservice.service";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-product_table',
@@ -42,13 +21,17 @@ export class Product_tableComponent implements OnInit {
   aux: Venta[] = []
   counterPage: number = 0;
 
+  dataSource: MatTableDataSource<Venta> = new MatTableDataSource<Venta>(this.products);
+  displayedColumns: string[] = ['select', 'id_producto', 'nombre', 'categoria', 'stock', 'precio'];
+  selection = new SelectionModel<Venta>(true, []);
+
   constructor(private http: HttpClient) { 
     
   }
 
-  ngOnInit() {
-    this.apicall();
-    this.comprobar();
+  async ngOnInit() {
+    await this.apicall();
+    this.dataSource = new MatTableDataSource<Venta>(this.products);
   }
 
   private async apicall(): Promise<void> {
@@ -79,20 +62,7 @@ export class Product_tableComponent implements OnInit {
     })));
   }
 
-  comprobar(): void {
-    let arreglo: string[][] = [['pepe', 'antonio', 'patricia'], ['rolando', 'jose', 'luis'], ['maria', 'laura', 'lucia']];
-
-    console.log(this.products);
-    
-    for (let i = 0; i < this.products.length; i++) {
-      console.log(this.products[i])
-    }
-    console.log(this.aux)
-  }
-
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
@@ -112,11 +82,11 @@ export class Product_tableComponent implements OnInit {
   }
 
   /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
+  checkboxLabel(row?: Venta): string {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id_producto + 1}`;
   }
 
 }
