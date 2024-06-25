fc846461e0f7ca82fbf380c57353735a64d25b92
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-06-24 20:37:37 -0700

    Completado envio de correos.

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -157,4 +157,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'elitestock970430@gmail.com'
-EMAIL_HOST_PASSWORD = 'elitestockcorreo01890014'
+EMAIL_HOST_PASSWORD = 'wngq ivqs rirf ccfy'
