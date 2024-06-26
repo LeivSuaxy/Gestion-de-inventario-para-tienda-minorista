1d10ab8d6e0e17271fe0294dbfa5ed7777adc62a
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-06-26 11:02:46 -0700

    Implements token_validate. Fixed error in column name in models.

--- Backend/login/urls.py
+++ Backend/login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, login
+from . import views
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('validate/', views.validate_token, name='validate_token'),
 ]
