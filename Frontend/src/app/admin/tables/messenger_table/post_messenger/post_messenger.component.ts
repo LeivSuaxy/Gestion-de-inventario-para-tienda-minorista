b36126af58891ed711aecdbfc76cd129ad7adf54
AGR55 <gonzalezreyesadriano@gmail.com>
2024-07-05 16:49:48 +0200

    cambios

--- Frontend/src/app/admin/tables/messenger_table/post_messenger/post_messenger.component.ts
+++ Frontend/src/app/admin/tables/messenger_table/post_messenger/post_messenger.component.ts
@@ -6,7 +6,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
@@ -21,7 +21,6 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatIconModule,
     MatButtonModule,
     RouterLink,
-    RouterLinkActive,
     MatCheckboxModule
   ],
   templateUrl: './post_messenger.component.html',
@@ -38,21 +37,15 @@ export class Post_messengerComponent {
 
   async posMethod(again: boolean): Promise<void> {
     let url = 'http://localhost:8000/api/admin/insert_messenger/';
-    const employeeData = {
-      employee: { // Añadir este nivel para encapsular los datos del empleado
-        ci: this.employee_ci ? this.employee_ci : '',
-        vehicle: this.vehicle ? this.vehicle: '',
-        salary_per_km: this.salary_per_km?.toString() ?? ''
-      }
-    };
+    const formData = new FormData();
+    formData.append('employee_ci', this.employee_ci ? this.employee_ci : '');
+    formData.append('vehicle', this.vehicle ? this.vehicle : '');
+    formData.append('salary_per_km', this.salary_per_km?.toString() ?? '');
 
     try {
       const response = await fetch(url, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(employeeData)
+        body: formData
       });
 
       if (!response.ok) {
@@ -64,11 +57,11 @@ export class Post_messengerComponent {
       this.employee_ci = this.vehicle = this.salary_per_km = undefined;
 
       if (!again) {
-        this.router.navigate(['/tables/messenger_table']);
+        this.cancel();
       } else {
         this.employee_ci = "";
-        this.salary_per_km = undefined;
         this.vehicle = "";
+        this.salary_per_km = undefined;
       }
     } catch (error) {
       console.error('There was a problem with your fetch operation:', error);
